10085264ae3469a8b0e3e88663801aaba0ee5025
francesharrison84 <42381987+francesharrison84@users.noreply.github.com>
2018-08-14 15:41:41 +0100

    hmmm

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-// create a function to get the current date & then return it as a string
+// JSX: create a function to get the current date & then return it as a string
+// - this is known as an arrow function
+// - identical to this kind of syntax:
+//      const getCurrentDaye = function() {
 const getCurrentDate = () => {
   const date = new Date();
   return date.toDateString();
@@ -25,4 +19,6 @@ const greeting = <h1>Hello World! Current Date: {getCurrentDate()}</h1>;
 // - react element to render
 // - DOM container to render the element in
 // - optional callback function
-ReactDOM.render(greeting, rootElement);
+// ReactDOM.render(greeting, rootElement);
+
+ReactDOM.render(<App />, rootElement);
