4e246d14e4e4d7199724b4842e80c5cfcdd44cd5
francesharrison84 <42381987+francesharrison84@users.noreply.github.com>
2018-08-14 15:35:06 +0100

    new function

--- src/index.js
+++ src/index.js
@@ -12,5 +12,17 @@ function App() {
   );
 }
 
+// create a function to get the current date & then return it as a string
+const getCurrentDate = () => {
+  const date = new Date();
+  return date.toDateString();
+};
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const greeting = <h1>Hello World! Current Date: {getCurrentDate()}</h1>;
+
+// render method takes 3 arguments:
+// - react element to render
+// - DOM container to render the element in
+// - optional callback function
+ReactDOM.render(greeting, rootElement);
